11a0796b4390ec38ebdc846033d3d5edcfa5cef0
Heisen-erg <heisenberg.database.1@gmail.com>
2023-09-10 13:30:29 +0530

    hh

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -33,6 +33,30 @@ const Project = () => {
                     </div>
                 </div>
             </div>
+
+            <div className="container-fluid">
+            <div className="row " >
+                <div className="col-12 m-0 p-0 "><img className='backg' src={require("../assets/followers.png")} alt="" /></div>
+                <div className="col-12 mt-3 p-0 customproject d-grid">
+
+                    <div className="car mt-2 z-2 d-grid ">
+                        <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-touch="false">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img src= {require("../assets/ss1.png")}  class="d-block w-100" alt="..." />
+                                </div>
+                        
+                                </div>
+                            </div>
+                           
+                        </div>
+                      <div className='center' > <Link to='https://followers.heisenberg.in.net/'><button className='btn mt-0 z-2 car1' >►</button> </Link> </div> 
+
+    
+
+                    </div>
+                </div>
+            </div>
         
             </>
     )
