eb4a435373fe749225992c80ac228932e856d563
Heisen-erg <heisenberg.database.1@gmail.com>
2023-09-10 13:32:11 +0530

    hh

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -36,14 +36,14 @@ const Project = () => {
 
             <div className="container-fluid">
             <div className="row " >
-                <div className="col-12 m-0 p-0 "><img className='backg' src={require("../assets/followers.png")} alt="" /></div>
+                <div className="col-12 m-0 p-0 "><img className='backg' src={require("../assets/project2.jpg")} alt="" /></div>
                 <div className="col-12 mt-3 p-0 customproject d-grid">
 
                     <div className="car mt-2 z-2 d-grid ">
                         <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-touch="false">
                             <div class="carousel-inner">
                                 <div class="carousel-item active">
-                                    <img src= {require("../assets/ss1.png")}  class="d-block w-100" alt="..." />
+                                    <img src= {require("../assets/followers.png")}  class="d-block w-100" alt="..." />
                                 </div>
                         
                                 </div>
